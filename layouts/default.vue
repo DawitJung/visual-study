1a68d767174184dde5d40c26d88e7ccd43052ed6
DawitJung <string.prototype.concat@gmail.com>
2021-03-15 00:51:39 +0900

    fix: git repo 업데이트

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-gray-200 min-h-screen">
-    <header class="w-full bg-white border-b shadow-md">
+    <header class="w-full bg-white shadow-md">
       <div
         class="w-full max-w-6xl mx-auto flex items-center justify-between p-4"
       >
@@ -8,7 +8,7 @@
           <nuxt-link to="/">비주얼 스터디 📐</nuxt-link>
         </div>
         <div>
-          <a href="https://github.com/" class="text-2xl">
+          <a href="https://github.com/DawitJung/visual-study" class="text-2xl">
             <i class="icon-github-circled"></i>
           </a>
         </div>
